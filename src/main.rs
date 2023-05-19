ba64df7d19a995ee84f8b2d306476da932ed7c9b
ejaaskel <esa.jaaskela@suomi24.fi>
2023-05-19 21:10:08 +0300

    Bind to named interface

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,50 @@
 use actix_web::{web, App, HttpServer, Result};
 use serde::Deserialize;
 
+use network_interface::NetworkInterface;
+use network_interface::NetworkInterfaceConfig;
+
 #[derive(Deserialize)]
 struct Info {
-    username: String,
+    ssid: String,
+    passphrase: String,
 }
 
-/// extract `Info` using serde
+fn print_hello(ssid: String, passphrase: String) {
+    println!("Received {:?} {:?}", ssid, passphrase);
+}
+
 async fn index(info: web::Json<Info>) -> Result<String> {
-    Ok(format!("Welcome {}!", info.username))
+    print_hello(info.ssid.clone(), info.passphrase.clone());
+    Ok(format!("You sent SSID {}!", info.ssid))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let network_interfaces = NetworkInterface::show().unwrap();
+
+    // Get ip4 address of the interface to bind to
+    let interface_to_bind_to = "wlan0";
+    let mut ip_to_bind_to = String::new();
+
+    for itf in network_interfaces.iter() {
+        if itf.name == interface_to_bind_to {
+            for address in itf.addr.iter() {
+                match address {
+                    network_interface::Addr::V4 (ip) => {
+                        println!("ipv4: {:?}", ip.ip);
+                        ip_to_bind_to = ip.ip.to_string();
+                    }
+                    network_interface::Addr::V6 (ip) => {
+                        println!("ipv6, not interested");
+                    }
+                }
+            }
+        }
+    }
+
     HttpServer::new(|| App::new().route("/", web::post().to(index)))
-        .bind(("127.0.0.1", 8080))?
+        .bind((ip_to_bind_to, 8080))?
         .run()
         .await
 }
